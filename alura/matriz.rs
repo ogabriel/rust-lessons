a1aa511d4368b05374a652f5fb2d016a116117c8
ogabriel <gabrieloliver8991@gmail.com>
2022-07-05 18:30:39 -0300

    add matriz

---
+++ alura/matriz.rs
@@ -0,0 +1,12 @@
+pub fn main() {
+    let matriz = [
+        [0.0, 1.2, 0.1],
+        [1.3, 0.3, 1.4]
+    ];
+
+    for linha in &matriz {
+        for item in linha {
+            println!("Item = {}", item);
+        }
+    }
+}
