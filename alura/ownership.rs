2da01e58c85cad714f208dc750e046543b658d99
ogabriel <gabrieloliver8991@gmail.com>
2022-07-05 17:03:33 -0300

    add ownership

---
+++ alura/ownership.rs
@@ -0,0 +1,10 @@
+pub fn main() {
+    let uma_string = String::from("vinicius");
+    rouba(uma_string);
+
+    println!("{}", uma_string);
+}
+
+fn rouba(string:String) {
+    println!("{}", string);
+}
