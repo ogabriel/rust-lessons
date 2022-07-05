0a04dceaad43d83280f155113cdd90b60e2f592d
ogabriel <gabrieloliver8991@gmail.com>
2022-07-05 17:52:43 -0300

    add erros

---
+++ alura/erros.rs
@@ -0,0 +1,16 @@
+pub fn main() {
+    // let v = vec![1, 2, 3];
+
+    // v[4];
+
+    // panic!("erro proposital");
+    match result() {
+        Ok(s) => println!("String de sucesso = {}", s),
+        Err(numero) => println!("Codigo de erro = {}", numero)
+    }
+}
+
+fn result() -> Result<String, u8> {
+    // Ok(String::from("Tudo deu certo"))
+    Err(42)
+}
