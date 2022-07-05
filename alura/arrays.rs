7b5a31349581a7d6aa3625098ef6b8d21ccfa395
ogabriel <gabrieloliver8991@gmail.com>
2022-07-05 18:31:13 -0300

    update arrays

--- alura/arrays.rs
+++ alura/arrays.rs
@@ -1,6 +1,16 @@
 pub fn main() {
-    let notas:[f32; 4] = [10f32, 8f32, 9.5, 6.0];
+    // let notas:[f32; 4] = [10f32, 8f32, 9.5, 6.0];
+    let notas:[f32; 4] = [6.5; 4];
 
-    println!("{}, {}, {} e {}", notas[0], notas[1], notas[2], notas[3]);
+    println!("--interar--");
+
+    for nota in &notas {
+        println!("A nota é = {}", nota);
+    }
+
+    println!("--indice--");
+
+    for i in 0..notas.len() {
+        println!("A nota {} é = {}", i + 1, notas[i]);
+    }
 }
-
