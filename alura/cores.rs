f31a958e7141e11e77c0fd6604d62a831525f15e
ogabriel <gabrieloliver8991@gmail.com>
2022-07-06 10:05:52 -0300

    add cores

---
+++ alura/cores.rs
@@ -0,0 +1,21 @@
+pub fn main() {
+    let cor = Color::CymkColor{cyan: 0, magenta: 0, yellow: 0, black: 255};
+
+    println!("Cor = {}", match cor {
+        Color::Red => "vermelho",
+        Color::Green => "verde",
+        Color::Blue => "azul",
+        Color::RgbColor(0, 0, 0) | Color::CymkColor{cyan: _, magenta: _, yellow: _, black: 255} => "preta",
+        Color::RgbColor(_, _, _) => "RGB desconhecido",
+        Color::CymkColor{cyan: _, magenta: _, yellow: _, black: _} => "CYMK desconhecido"
+    });
+}
+
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8),
+    CymkColor{cyan: u8, magenta: u8, yellow: u8, black: u8}
+}
