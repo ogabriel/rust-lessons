1e3a8d772e0854b50390129a6eaf4d29f393c34d
ogabriel <gabrieloliver8991@gmail.com>
2022-07-06 11:14:31 -0300

    add vectors

---
+++ alura/vectors.rs
@@ -0,0 +1,15 @@
+pub fn main() {
+    // let mut notas: Vec<f32> = Vec::new();
+
+    // notas.push(10.0);
+    // notas.push(5.5);
+    // notas.push(7.1);
+
+    // println!("{:?}", notas);
+
+    // notas.push(8.1);
+
+    let notas: Vec<f32> = vec![10.0, 5.5, 7.1, 8.1];
+
+    println!("{:?}", notas);
+}
